1ff16393feb8d891cf242d6513663b6ca04de3ab
Lucas Carlos Bento <38960407+LucasCarlos@users.noreply.github.com>
2022-08-08 22:08:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# NASA Mission Control Project
+
+NASA project from [Course](https://www.udemy.com/course/complete-nodejs-developer-zero-to-mastery/).
+
+## Getting Started
+
+1. Ensure you have Node.js installed.
+2. Create a free [Mongo Atlas](https://www.mongodb.com/atlas/database) database online or start a local MongoDB database.
+3. Create a `server/.env` file with a `MONGO_URL` property set to your MongoDB connection string.
+4. In the terminal, run: `npm install`
+
+## Running the Project
+
+1. In the terminal, run: `npm run deploy`
+2. Browse to the mission control frontend at [localhost:8000](http://localhost:8000).
---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    require: true,
+  },
+  launchDate: {
+    type: Date,
+    require: true,
+  },
+  mission: {
+    type: String,
+    require: true,
+  },
+  rocket: {
+    type: String,
+    require: true,
+  },
+  target: {
+    type: String,
+  },
+  customers: [String],
+  upcoming: {
+    type: Boolean,
+    require: true,
+  },
+  success: {
+    type: Boolean,
+    require: true,
+    default: true,
+  },
+});
+
+//Connects launchesSchema with the "launches" collection
+module.exports = mongoose.model("Launch", launchesSchema);
